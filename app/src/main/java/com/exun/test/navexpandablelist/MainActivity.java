201c7611cded7ebf5b602b31c513f2ed0674fcab
D8thReaper <ayushpawha96@gmail.com>
2015-12-13 15:29:16 +0530

    Added dynamic implementation for data addition

--- app/src/main/java/com/exun/test/navexpandablelist/MainActivity.java
+++ app/src/main/java/com/exun/test/navexpandablelist/MainActivity.java
@@ -1,5 +1,6 @@
 package com.exun.test.navexpandablelist;
 
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -10,6 +11,7 @@ import android.widget.ExpandableListView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 
@@ -78,39 +80,45 @@ public class MainActivity extends AppCompatActivity implements ExpandableListVie
         listDataHeader = new ArrayList<String>();
         listDataChild = new HashMap<String, List<String>>();
 
-        // Adding child data
-        listDataHeader.add("Top 250");
-        listDataHeader.add("Now Showing");
-        listDataHeader.add("Coming Soon..");
 
-        // Adding child data
-        List<String> top250 = new ArrayList<String>();
-        top250.add("The Shawshank Redemption");
-        top250.add("The Godfather");
-        top250.add("The Godfather: Part II");
-        top250.add("Pulp Fiction");
-        top250.add("The Good, the Bad and the Ugly");
-        top250.add("The Dark Knight");
-        top250.add("12 Angry Men");
+        // Adding headers
+        Resources res = getResources();
+        String[] headers = res.getStringArray(R.array.nav_drawer_labels);
+        listDataHeader = Arrays.asList(headers);
 
-        List<String> nowShowing = new ArrayList<String>();
-        nowShowing.add("The Conjuring");
-        nowShowing.add("Despicable Me 2");
-        nowShowing.add("Turbo");
-        nowShowing.add("Grown Ups 2");
-        nowShowing.add("Red 2");
-        nowShowing.add("The Wolverine");
+        //Adding child data
 
-        List<String> comingSoon = new ArrayList<String>();
-        comingSoon.add("2 Guns");
-        comingSoon.add("The Smurfs 2");
-        comingSoon.add("The Spectacular Now");
-        comingSoon.add("The Canyons");
-        comingSoon.add("Europa Report");
+//        //Dynamic method
+//        for (int i =0; i<listDataHeader.size(); i++){
+//
+//            //Save data in array
+//            String[] childData = res.getStringArray(R.array.elements_home);
+//
+//            //Put data in List
+//            List<String> listChild;
+//            listChild = Arrays.asList(childData);
+//
+//            //Add to hashMap
+//            listDataChild.put(listDataHeader.get(i),listChild);
+//        }
 
-        listDataChild.put(listDataHeader.get(0), top250); // Header, Child data
-        listDataChild.put(listDataHeader.get(1), nowShowing);
-        listDataChild.put(listDataHeader.get(2), comingSoon);
+        // Static method
+        List<String> home,friends,notifs;
+        String[] shome,sfriends,snotifs;
+
+        shome = res.getStringArray(R.array.elements_home);
+        home = Arrays.asList(shome);
+
+        sfriends = res.getStringArray(R.array.elements_friends);
+        friends = Arrays.asList(sfriends);
+
+        snotifs = res.getStringArray(R.array.elements_notifs);
+        notifs = Arrays.asList(snotifs);
+
+        // Add to hashMap
+        listDataChild.put(listDataHeader.get(0), home); // Header, Child data
+        listDataChild.put(listDataHeader.get(1), friends);
+        listDataChild.put(listDataHeader.get(2), notifs);
     }
 
     @Override
